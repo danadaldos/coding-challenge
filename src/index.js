684b5707367006bc626262acb317737d6d3fbd0e
Karl Berggren <karl@wonderschool.com>
2021-05-19 16:50:35 -0700

    Added apollo client for GQL queries and an example query for the data required.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from '@apollo/client/react';
+import { ApolloClient, InMemoryCache} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: '/graphiql',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
